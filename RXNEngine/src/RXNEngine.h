3f2c9ae571b5735bf841607eb3c08cc4754f8e6a
RxNaison <87330825+RxNaison@users.noreply.github.com>
2025-05-14 21:09:31 +0700

    key codes added

--- RXNEngine/src/RXNEngine.h
+++ RXNEngine/src/RXNEngine.h
@@ -4,6 +4,10 @@
 #include "RXNEngine/Layer.h"
 #include "RXNEngine/Log.h"
 
+#include "RXNEngine/Input.h"
+#include "RXNEngine/KeyCodes.h"
+#include "RXNEngine/MouseButtonCodes.h"
+
 #include "RXNEngine/ImGui/ImGuiLayer.h"
 
 #include "RXNEngine/EntryPoint.h"
---
+++ RXNEngine/src/RXNEngine/KeyCodes.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#define RXN_KEY_SPACE              32
+#define RXN_KEY_APOSTROPHE         39  /* ' */
+#define RXN_KEY_COMMA              44  /* , */
+#define RXN_KEY_MINUS              45  /* - */
+#define RXN_KEY_PERIOD             46  /* . */
+#define RXN_KEY_SLASH              47  /* / */
+#define RXN_KEY_0                  48
+#define RXN_KEY_1                  49
+#define RXN_KEY_2                  50
+#define RXN_KEY_3                  51
+#define RXN_KEY_4                  52
+#define RXN_KEY_5                  53
+#define RXN_KEY_6                  54
+#define RXN_KEY_7                  55
+#define RXN_KEY_8                  56
+#define RXN_KEY_9                  57
+#define RXN_KEY_SEMICOLON          59  /* ; */
+#define RXN_KEY_EQUAL              61  /* = */
+#define RXN_KEY_A                  65
+#define RXN_KEY_B                  66
+#define RXN_KEY_C                  67
+#define RXN_KEY_D                  68
+#define RXN_KEY_E                  69
+#define RXN_KEY_F                  70
+#define RXN_KEY_G                  71
+#define RXN_KEY_H                  72
+#define RXN_KEY_I                  73
+#define RXN_KEY_J                  74
+#define RXN_KEY_K                  75
+#define RXN_KEY_L                  76
+#define RXN_KEY_M                  77
+#define RXN_KEY_N                  78
+#define RXN_KEY_O                  79
+#define RXN_KEY_P                  80
+#define RXN_KEY_Q                  81
+#define RXN_KEY_R                  82
+#define RXN_KEY_S                  83
+#define RXN_KEY_T                  84
+#define RXN_KEY_U                  85
+#define RXN_KEY_V                  86
+#define RXN_KEY_W                  87
+#define RXN_KEY_X                  88
+#define RXN_KEY_Y                  89
+#define RXN_KEY_Z                  90
+#define RXN_KEY_LEFT_BRACKET       91  /* [ */
+#define RXN_KEY_BACKSLASH          92  /* \ */
+#define RXN_KEY_RIGHT_BRACKET      93  /* ] */
+#define RXN_KEY_GRAVE_ACCENT       96  /* ` */
+#define RXN_KEY_WORLD_1            161 /* non-US #1 */
+#define RXN_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define RXN_KEY_ESCAPE             256
+#define RXN_KEY_ENTER              257
+#define RXN_KEY_TAB                258
+#define RXN_KEY_BACKSPACE          259
+#define RXN_KEY_INSERT             260
+#define RXN_KEY_DELETE             261
+#define RXN_KEY_RIGHT              262
+#define RXN_KEY_LEFT               263
+#define RXN_KEY_DOWN               264
+#define RXN_KEY_UP                 265
+#define RXN_KEY_PAGE_UP            266
+#define RXN_KEY_PAGE_DOWN          267
+#define RXN_KEY_HOME               268
+#define RXN_KEY_END                269
+#define RXN_KEY_CAPS_LOCK          280
+#define RXN_KEY_SCROLL_LOCK        281
+#define RXN_KEY_NUM_LOCK           282
+#define RXN_KEY_PRINT_SCREEN       283
+#define RXN_KEY_PAUSE              284
+#define RXN_KEY_F1                 290
+#define RXN_KEY_F2                 291
+#define RXN_KEY_F3                 292
+#define RXN_KEY_F4                 293
+#define RXN_KEY_F5                 294
+#define RXN_KEY_F6                 295
+#define RXN_KEY_F7                 296
+#define RXN_KEY_F8                 297
+#define RXN_KEY_F9                 298
+#define RXN_KEY_F10                299
+#define RXN_KEY_F11                300
+#define RXN_KEY_F12                301
+#define RXN_KEY_F13                302
+#define RXN_KEY_F14                303
+#define RXN_KEY_F15                304
+#define RXN_KEY_F16                305
+#define RXN_KEY_F17                306
+#define RXN_KEY_F18                307
+#define RXN_KEY_F19                308
+#define RXN_KEY_F20                309
+#define RXN_KEY_F21                310
+#define RXN_KEY_F22                311
+#define RXN_KEY_F23                312
+#define RXN_KEY_F24                313
+#define RXN_KEY_F25                314
+#define RXN_KEY_KP_0               320
+#define RXN_KEY_KP_1               321
+#define RXN_KEY_KP_2               322
+#define RXN_KEY_KP_3               323
+#define RXN_KEY_KP_4               324
+#define RXN_KEY_KP_5               325
+#define RXN_KEY_KP_6               326
+#define RXN_KEY_KP_7               327
+#define RXN_KEY_KP_8               328
+#define RXN_KEY_KP_9               329
+#define RXN_KEY_KP_DECIMAL         330
+#define RXN_KEY_KP_DIVIDE          331
+#define RXN_KEY_KP_MULTIPLY        332
+#define RXN_KEY_KP_SUBTRACT        333
+#define RXN_KEY_KP_ADD             334
+#define RXN_KEY_KP_ENTER           335
+#define RXN_KEY_KP_EQUAL           336
+#define RXN_KEY_LEFT_SHIFT         340
+#define RXN_KEY_LEFT_CONTROL       341
+#define RXN_KEY_LEFT_ALT           342
+#define RXN_KEY_LEFT_SUPER         343
+#define RXN_KEY_RIGHT_SHIFT        344
+#define RXN_KEY_RIGHT_CONTROL      345
+#define RXN_KEY_RIGHT_ALT          346
+#define RXN_KEY_RIGHT_SUPER        347
+#define RXN_KEY_MENU               348
