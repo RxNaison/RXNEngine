0bc8cd4b10703e7115a3b4859e91ab3a1f036e49
RxNaison <87330825+RxNaison@users.noreply.github.com>
2025-08-23 18:00:33 +0700

    RendererAPI extented

--- RXNEngine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ RXNEngine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -27,9 +27,22 @@ namespace RXNEngine {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
-	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray)
+	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount)
 	{
-		glDrawElements(GL_TRIANGLES, vertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+		vertexArray->Bind();
+		uint32_t count = indexCount ? indexCount : vertexArray->GetIndexBuffer()->GetCount();
+		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
+	}
+
+	void OpenGLRendererAPI::DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount)
+	{
+		vertexArray->Bind();
+		glDrawArrays(GL_LINES, 0, vertexCount);
+	}
+
+	void OpenGLRendererAPI::SetLineWidth(float width)
+	{
+		glLineWidth(width);
 	}
 
 }
--- RXNEngine/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ RXNEngine/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -13,7 +13,10 @@ namespace RXNEngine {
 		virtual void SetClearColor(const glm::vec4& color) override;
 		virtual void Clear() override;
 
-		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray) override;
+		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) override;
+		virtual void DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount) override;
+
+		void SetLineWidth(float width) override;
 	};
 
 }
