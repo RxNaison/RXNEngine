16509d5e38ce4af791036e4e123e5bc264085461
RxNaison <87330825+RxNaison@users.noreply.github.com>
2025-08-24 00:48:27 +0700

    Texture update

--- RXNEngine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ RXNEngine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -1,19 +1,63 @@
 #include "rxnpch.h"
 #include "OpenGLTexture.h"
 
-#include "glad/glad.h"
-#include "stb_image.h"
+#include <stb_image.h>
 
-namespace RXNEngine{
+namespace RXNEngine {
+
+	namespace Utils {
+
+		static GLenum ImageFormatToGLDataFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB8:  return GL_RGB;
+				case ImageFormat::RGBA8: return GL_RGBA;
+			}
+
+			RXN_CORE_ASSERT(false);
+			return 0;
+		}
+
+		static GLenum ImageFormatToGLInternalFormat(ImageFormat format)
+		{
+			switch (format)
+			{
+				case ImageFormat::RGB8:  return GL_RGB8;
+				case ImageFormat::RGBA8: return GL_RGBA8;
+			}
+
+			RXN_CORE_ASSERT(false);
+			return 0;
+		}
+
+	}
+
+	OpenGLTexture2D::OpenGLTexture2D(const TextureSpecification& specification)
+		: m_Specification(specification), m_Width(m_Specification.Width), m_Height(m_Specification.Height)
+	{
+		m_InternalFormat = Utils::ImageFormatToGLInternalFormat(m_Specification.Format);
+		m_DataFormat = Utils::ImageFormatToGLDataFormat(m_Specification.Format);
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
 
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
-		:m_Path(path)
+		: m_Path(path)
 	{
 		int width, height, channels;
 		stbi_set_flip_vertically_on_load(1);
-		stbi_uc* data = stbi_load(path.c_str(), &width, &height, &channels, 0);
+		stbi_uc* data = nullptr;
+		data = stbi_load(path.c_str(), &width, &height, &channels, 0);
 
-		if(data)
+		if (data)
 		{
 			m_IsLoaded = true;
 
@@ -35,8 +79,6 @@ namespace RXNEngine{
 			m_InternalFormat = internalFormat;
 			m_DataFormat = dataFormat;
 
-			RXN_CORE_ASSERT(internalFormat & dataFormat, "Format not supported!");
-
 			glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
 			glTextureStorage2D(m_RendererID, 1, internalFormat, m_Width, m_Height);
 
@@ -57,9 +99,15 @@ namespace RXNEngine{
 		glDeleteTextures(1, &m_RendererID);
 	}
 
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bytesPerPixel = m_DataFormat == GL_RGBA ? 4 : 3;
+		RXN_CORE_ASSERT(size == m_Width * m_Height * bytesPerPixel, "Data must be entire texture!");
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
+	}
+
 	void OpenGLTexture2D::Bind(uint32_t slot) const
 	{
 		glBindTextureUnit(slot, m_RendererID);
 	}
-
 }
