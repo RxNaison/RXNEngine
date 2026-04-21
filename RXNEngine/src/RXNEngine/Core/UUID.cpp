38025374a5ace765d01a07374ebdeaed45911aa0
RxNaison <87330825+RxNaison@users.noreply.github.com>
2026-04-21 18:57:39 +0700

    UUID Data Race Fix

--- RXNEngine/src/RXNEngine/Core/UUID.cpp
+++ RXNEngine/src/RXNEngine/Core/UUID.cpp
@@ -6,8 +6,8 @@
 namespace RXNEngine {
 
 	static std::random_device s_RandomDevice;
-	static std::mt19937_64 s_Engine(s_RandomDevice());
-	static std::uniform_int_distribution<uint64_t> s_UniformDistribution;
+	static thread_local std::mt19937_64 s_Engine(s_RandomDevice());
+	static thread_local std::uniform_int_distribution<uint64_t> s_UniformDistribution;
 
 	UUID::UUID()
 	{
